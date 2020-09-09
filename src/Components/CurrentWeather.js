011838203eba8e8a33176a888e64b4a92ee59e0e
Mina <mina.park127@gmail.com>
2020-09-09 17:28:30 +1000

    styling currentWeather

--- src/Components/CurrentWeather.js
+++ src/Components/CurrentWeather.js
@@ -1,6 +1,84 @@
 import React from "react";
 import styled from "styled-components";
 
+const Wrapper = styled.div`
+  background-color: rgb(45, 52, 54);
+  color: #dfe6e9;
+  width: 100%;
+  height: 35vh;
+  padding: 30px;
+  display: grid;
+  grid-template-columns: 0.8fr 1.5fr;
+`;
+const Current = styled.section`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  column-gap: 10px;
+  dl:first-of-type {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    dt {
+      text-transform: capitalize;
+      font-size: 18px;
+    }
+    dd {
+      width: 60%;
+      img {
+        width: 100%;
+      }
+    }
+  }
+  dl:nth-of-type(2) {
+    height: 100%;
+    display: grid;
+    grid-template-rows: repeat(2, 1fr);
+    align-items: center;
+    div {
+      line-height: 2;
+      dt {
+        width: 80%;
+        font-size: 14px;
+        text-transform: uppercase;
+        opacity: 0.8;
+        margin-bottom: 10px;
+        border-bottom: 1px solid rgba(223, 230, 233, 0.8);
+      }
+      dd {
+        font-size: 30px;
+      }
+    }
+  }
+`;
+
+const Items = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+`;
+
+const Item = styled.dl`
+  display: grid;
+  grid-template-rows: 2.5fr repeat(3, 1fr);
+  align-items: center;
+  justify-items: center;
+  svg {
+    stroke: #dfe6e9;
+    width: 40%;
+    height: 40%;
+    align-self: end;
+  }
+  dt {
+    font-size: 16px;
+    text-transform: uppercase;
+    opacity: 0.8;
+  }
+  dd {
+    font-size: 25px;
+  }
+`;
+
 export default ({ currentWeather }) => {
   const iconID = currentWeather.weather[0].icon;
   const iconURL = `http://openweathermap.org/img/wn/${iconID}@2x.png`;
@@ -26,34 +104,34 @@ export default ({ currentWeather }) => {
     }
   };
   return (
-    <div>
+    <Wrapper>
       {currentWeather && (
         <>
-          <section>
+          <Current>
             <dl>
-              <dt>Today</dt>
               <dd>
                 <img src={iconURL} alt="current weather icon" />
               </dd>
-              <dd>{currentWeather.weather[0].description}</dd>
+              <dt>{currentWeather.weather[0].description}</dt>
             </dl>
             <dl>
-              <dt>Current</dt>
-              <dd>{Math.floor(currentWeather.temp)}°c</dd>
-              <dt>Feels like</dt>
-              <dd>{Math.floor(currentWeather.feels_like)}°c</dd>
+              <div>
+                <dt>Current</dt>
+                <dd>{Math.floor(currentWeather.temp)}°c</dd>
+              </div>
+              <div>
+                <dt>Feels like</dt>
+                <dd>{Math.floor(currentWeather.feels_like)}°c</dd>
+              </div>
             </dl>
-          </section>
-          <div>
-            <dl>
+          </Current>
+          <Items>
+            <Item>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="icon icon-tabler icon-tabler-droplet"
-                width="40"
-                height="40"
                 viewBox="0 0 24 24"
                 strokeWidth="1"
-                stroke="#2c3e50"
                 fill="none"
                 strokeLinecap="round"
                 strokeLinejoin="round"
@@ -62,17 +140,15 @@ export default ({ currentWeather }) => {
                 <path d="M12 3l5 5a7 7 0 1 1 -10 0l5 -5" />
               </svg>
               <dt>Humidity</dt>
+              <span>―</span>
               <dd>{Math.floor(currentWeather.humidity)}%</dd>
-            </dl>
-            <dl>
+            </Item>
+            <Item>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="icon icon-tabler icon-tabler-brightness-up"
-                width="40"
-                height="40"
                 viewBox="0 0 24 24"
                 strokeWidth="1"
-                stroke="#2c3e50"
                 fill="none"
                 strokeLinecap="round"
                 strokeLinejoin="round"
@@ -89,17 +165,15 @@ export default ({ currentWeather }) => {
                 <line x1="7" y1="7" x2="5.6" y2="5.6" />
               </svg>
               <dt>UV Index</dt>
+              <span>―</span>
               <dd>{Math.floor(currentWeather.uvi)}</dd>
-            </dl>
-            <dl>
+            </Item>
+            <Item>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="icon icon-tabler icon-tabler-sunset"
-                width="40"
-                height="40"
                 viewBox="0 0 24 24"
                 strokeWidth="1"
-                stroke="#2c3e50"
                 fill="none"
                 strokeLinecap="round"
                 strokeLinejoin="round"
@@ -110,17 +184,15 @@ export default ({ currentWeather }) => {
                 <path d="M12 9v-6l3 3m-6 0l3 -3" />
               </svg>
               <dt>Sunrise</dt>
+              <span>―</span>
               <dd>{getTime("sunrise")}</dd>
-            </dl>
-            <dl>
+            </Item>
+            <Item>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="icon icon-tabler icon-tabler-sunshine"
-                width="40"
-                height="40"
                 viewBox="0 0 24 24"
                 strokeWidth="1"
-                stroke="#2c3e50"
                 fill="none"
                 strokeLinecap="round"
                 strokeLinejoin="round"
@@ -131,11 +203,12 @@ export default ({ currentWeather }) => {
                 <path d="M12 3v6l3 -3m-6 0l3 3" />
               </svg>
               <dt>Sunset</dt>
+              <span>―</span>
               <dd>{getTime("sunset")}</dd>
-            </dl>
-          </div>
+            </Item>
+          </Items>
         </>
       )}
-    </div>
+    </Wrapper>
   );
 };
