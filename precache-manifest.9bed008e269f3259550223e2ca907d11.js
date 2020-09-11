1ce8c0352832007d5b2e8b2df272e51a43e3a5eb
Mina <mina.park127@gmail.com>
2020-09-11 18:19:52 +1000

    Updates

---
+++ precache-manifest.9bed008e269f3259550223e2ca907d11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ec63e7e9b6fa9d1a08f6f5a274ee86a",
+    "url": "/is_it_sunny/index.html"
+  },
+  {
+    "revision": "20f68453fc6524bc9e57",
+    "url": "/is_it_sunny/static/js/2.e17b9999.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/is_it_sunny/static/js/2.e17b9999.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4af5c4759b8942a49231",
+    "url": "/is_it_sunny/static/js/main.d038ba6b.chunk.js"
+  },
+  {
+    "revision": "db6bddb6e2ce91f232be",
+    "url": "/is_it_sunny/static/js/runtime-main.fbfa2b57.js"
+  }
+]);
